3ae2e0e957b8c71d75933c66e76ca7ac00ddb182
Gil Barbara <gilbarbara@gmail.com>
2019-01-01 01:46:28 -0200

    Update demos steps

--- src/Scroll/index.js
+++ src/Scroll/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import ReactJoyride from "react-joyride";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import ReactJoyride from 'react-joyride';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   align-items: center;
@@ -36,49 +36,25 @@ class ScrollDemo extends Component {
     run: true,
     steps: [
       {
-        content: "Our awesome projects",
-        placement: "bottom",
-        target: ".app__scroller h1",
-        textAlign: "center"
+        target: '.app__scroller h1',
+        content: 'The latest version of React!',
+        placement: 'bottom',
+        textAlign: 'center'
       },
       {
-        content:
-          "Can be advanced by clicking an element through the overlay hole.",
-        disableCloseOnEsc: true,
-        disableOverlayClicks: true,
-        placement: "top",
-        target: ".app__scroller h3:nth-of-type(2)",
-        title: "Our Mission"
+        target: '.app__scroller h3:nth-of-type(2)',
+        content: 'Nobody likes errors! 🤬',
+        placement: 'top',
       },
       {
-        content: "This step tests what happens when a target is missing",
-        target: ".app__scroller h3:nth-of-type(3)",
-        title: "Unmounted target",
-        placement: "top"
+        target: '.app__scroller h3:nth-of-type(3)',
+        content: 'Yay! Portals are awesome',
+        placement: 'top'
       },
       {
-        content: (
-          <div>
-            <h3>We are the people</h3>
-            <svg
-              width="96px"
-              height="96px"
-              viewBox="0 0 96 96"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid"
-            >
-              <g>
-                <path
-                  d="M83.2922435,72.3864207 C69.5357835,69.2103145 56.7313553,66.4262214 62.9315626,54.7138297 C81.812194,19.0646376 67.93573,0 48.0030634,0 C27.6743835,0 14.1459311,19.796662 33.0745641,54.7138297 C39.4627778,66.4942237 26.1743334,69.2783168 12.7138832,72.3864207 C0.421472164,75.2265157 -0.0385432192,81.3307198 0.0014581185,92.0030767 L0.0174586536,96.0032105 L95.9806678,96.0032105 L95.9966684,92.1270809 C96.04467,81.3747213 95.628656,75.2385161 83.2922435,72.3864207 Z"
-                  fill="#000000"
-                />
-              </g>
-            </svg>
-          </div>
-        ),
-        target: ".app__scroller h3:nth-of-type(4)",
-        placement: "top"
+        target: '.app__scroller h3:nth-of-type(4)',
+        content: 'SSR is supported',
+        placement: 'top'
       }
     ]
   };
